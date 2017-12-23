1cf33d7cb57ebdbcfe7af799f41e0c224d84f589
Leo Cooper <Leo Cooper>
2017-12-23 20:21:16 +0800

    添加项目文件。

---
+++ Points/源.cpp
@@ -0,0 +1,19 @@
+//Point_manager ver_alpha_0.1 By_leocooper
+#include <iostream>
+using namespace std;
+void printer();
+void newcust();
+int check();
+int main()
+{
+	printer();
+	
+
+}
+void printer() 
+{
+	cout << "-------------------------------------------------------------------------------------" << endl;
+	cout << "                           Point_manager ver_alpha_0.1 By_leocooper " << endl;
+	cout << "--------------------------------------------------------------------------------------" << endl;
+	cout << "loding";
+}
